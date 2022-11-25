2a920d2ea63cd05559d5f2534712b38f3588607b
Evgeny Shestakov <evgeny.shestakov@techusage.com>
2022-11-25 15:31:10 +0100

    fix run

--- api/run.py
+++ api/run.py
@@ -1,9 +1,9 @@
 import uvicorn
 
-from api_trading.create_app import create_app
+from api.create_app import create_app
 from config import config
 
 app = create_app(config)
 
 if __name__ == "__main__":
-    uvicorn.run("api_trading.run:app", host="0.0.0.0", port=8000, workers=config.WORKERS, reload=config.IS_DEBUG)
+    uvicorn.run("api.run:app", host="0.0.0.0", port=8000, workers=config.WORKERS, reload=config.IS_DEBUG)
