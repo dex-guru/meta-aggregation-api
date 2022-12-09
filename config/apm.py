221a3b9ac9ba974c32a22657b71b90bde67830d8
Evgeny <shestakoven@yandex.ru>
2022-12-09 13:58:13 +0100

    add service name
    Co-authored-by: Eugene Safrankov <eugene.safrankov@deltatre.com>

--- config/apm.py
+++ config/apm.py
@@ -2,8 +2,8 @@ from pydantic import BaseSettings, HttpUrl
 
 
 class APMConfig(BaseSettings):
-    APM_SERVER_URL: HttpUrl = 'https://apm-stage-lax.dexguru.biz:443'
-    SERVICE_NAME: str = 'UNSPECIFIED SERVICE'
+    APM_SERVER_URL: HttpUrl = ''
+    SERVICE_NAME: str = 'api-trading'
     APM_ENABLED: bool = False
     APM_RECORDING: bool = False
     APM_CAPTURE_HEADERS: bool = False
