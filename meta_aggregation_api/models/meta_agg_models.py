4aaba505353d76e25f391bcb7f71e8bcb96aef86
Evgeny Shestakov <evgeny.shestakov@techusage.com>
2022-12-21 15:01:02 +0100

    add endpoint for limit order post

--- meta_aggregation_api/models/meta_agg_models.py
+++ meta_aggregation_api/models/meta_agg_models.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from meta_aggregation_api.models.provider_response_models import SwapSources
 
@@ -35,3 +35,27 @@ class ProviderQuoteResponse(BaseModel):
     gas_price: str  # gas price for the swap
     value: str  # amount of native token that should be sent with the transaction
     price: str  # price for buy_token in sell_token
+
+
+class LimitOrderPostData(BaseModel):
+    makerAsset: str = Field(
+        ..., description='The address of maker token', alias='maker_asset'
+    )
+    takerAsset: str = Field(
+        ..., description='The address of taker token', alias='taker_asset'
+        )
+    maker: str = Field(..., description='The address of maker')
+    allowedSender: str = Field(
+        ..., description='The address of allowed sender', alias='allowed_sender'
+    )
+    receiver: str = Field(..., description='The address of receiver')
+    makingAmount: str = Field(
+        ..., description='The amount of maker token', alias='making_amount'
+    )
+    takingAmount: str = Field(
+        ..., description='The amount of taker token', alias='taking_amount')
+    salt: str = Field(..., description='The salt of the order')
+    interactions: Optional[str] = Field('0x', description='The interactions of the order')
+
+    class Config:
+        allow_population_by_field_name = True
