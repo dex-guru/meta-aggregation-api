c2c0c6aaf40f9101e54697cf54a4fc2c508b01fb
Evgeny <evgeny.shestakov@techusage.com>
2023-02-09 18:34:14 +0100

    add gasInclude param for kyberswap (#21)
    * add gasInclude param for kyberswap
    
    * fix clientData

--- meta_aggregation_api/providers/kyberswap_v1/kyberswap_provider_v1.py
+++ meta_aggregation_api/providers/kyberswap_v1/kyberswap_provider_v1.py
@@ -212,7 +212,8 @@ class KyberSwapProviderV1(BaseProvider):
             'tokenIn': sell_token,
             'tokenOut': buy_token,
             'amountIn': str(sell_amount),
-            'clientData': "{'source': '%s'}" % self.config.PARTNER,
+            'clientData': '{"source": "%s"}' % self.config.PARTNER,
+            'gasInclude': 1,
         }
         if taker_address:
             params['to'] = taker_address
