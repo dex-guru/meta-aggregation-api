9ec36fc357d6d667ffa161b07e1b470054908300
Evgeny Shestakov <evgeny.shestakov@techusage.com>
2023-06-14 15:07:20 +0200

    cache rpc requests

--- meta_aggregation_api/rest_api/routes/rpc.py
+++ meta_aggregation_api/rest_api/routes/rpc.py
@@ -1,6 +1,7 @@
 import ssl
 
 import aiohttp
+from aiocache import cached
 from aiohttp import ClientResponseError
 from fastapi import APIRouter, Depends, HTTPException, Path
 from fastapi.security import HTTPBearer
@@ -9,6 +10,7 @@ from starlette.requests import Request
 
 from meta_aggregation_api.rest_api import dependencies
 from meta_aggregation_api.rest_api.dependencies import aiohttp_session
+from meta_aggregation_api.utils.cache import get_cache_config
 from meta_aggregation_api.utils.common import get_web3_url
 from meta_aggregation_api.utils.logger import get_logger
 
@@ -29,12 +31,23 @@ async def send_rpc(
     that is currently synced with the most other nodes. It takes in a JSON-RPC 2.0 compliant
     request and returns a JSON-RPC 2.0 compliant response.
     """
+
     authorize.jwt_required()
     node = get_web3_url(chain_id, config)
-    try:
-        async with session.post(
-            node, proxy=None, json=await request.json(), ssl=ssl.SSLContext()
-        ) as response:
-            return await response.json()
-    except ClientResponseError as e:
-        raise HTTPException(status_code=e.status, detail=e.message)
+    body = await request.json()
+    if body.get('method') == 'eth_chainId':
+        return {'jsonrpc': '2.0', 'id': body['id'], 'result': hex(chain_id)}
+    if body.get('method') == 'net_version':
+        return {'jsonrpc': '2.0', 'id': body['id'], 'result': str(chain_id)}
+
+    @cached(ttl=5, **get_cache_config(config))
+    async def make_request(node_, body_):
+        try:
+            async with session.post(
+                node_, proxy=None, json=body_, ssl=ssl.SSLContext()
+            ) as response:
+                return await response.json()
+        except ClientResponseError as e:
+            raise HTTPException(status_code=e.status, detail=e.message)
+
+    return await make_request(node, body)
--- meta_aggregation_api/utils/cache.py
+++ meta_aggregation_api/utils/cache.py
@@ -3,6 +3,7 @@ from hashlib import md5
 
 from aiocache import Cache
 from aiocache.serializers import PickleSerializer
+from aiohttp import ClientSession
 from starlette.requests import Request
 from web3.contract import AsyncContract
 
@@ -12,12 +13,14 @@ from meta_aggregation_api.config import CacheConfig
 def key_from_args(func, *args, **kwargs):
     filtered_args = []
     for arg in args:
-        if isinstance(arg, Enum):
+        if isinstance(arg, Request):
+            continue
+        elif isinstance(arg, ClientSession):
+            continue
+        elif isinstance(arg, Enum):
             filtered_args.append(arg.value)
         elif isinstance(arg, AsyncContract):
             filtered_args.append(arg.address)
-        elif isinstance(arg, Request):
-            continue
         elif hasattr(arg, 'PROVIDER_NAME'):
             filtered_args.append(arg.PROVIDER_NAME)
         filtered_args.append(arg)
